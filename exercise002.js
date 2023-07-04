337ccee9e06aa7aee9ff150f57e4c077532f72ed
Zulfu <zulfu.yilmaz.a@gmail.com>
2023-07-04 22:29:18 +0100

    Refactor fetchData function to use async and await -  try and catch

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,14 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+const fetchData = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 fetchData(jsonTypicode);
